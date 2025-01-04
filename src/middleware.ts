412a9f9c1ae69ad230673dea64ccc01f42c9503a
GroupResearchProject <mohdtoufeeq1447@gmail.com>
2025-01-04 22:29:20 +0000

    fixing ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,18 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('auth-token'); // Assuming your token is stored in a cookie named 'auth-token'
+  const token = request.cookies.get('auth-token');
   const url = request.nextUrl;
-
-  // Redirect authenticated users to the dashboard if they try to access sign-in, sign-up, verify, or home
+  if (!token && url.pathname.startsWith('/u')) {
+    return NextResponse.next();
+  }
   if (token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/verify') || url.pathname === '/')) {
-    return NextResponse.redirect(new URL('/dashboard', request.url)); // Redirect to /dashboard if logged in
+    return NextResponse.redirect(new URL('/dashboard', request.url)); 
   }
 
-  // Redirect unauthenticated users to the home page if they try to access protected routes (e.g., /dashboard)
   if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/home', request.url)); // Redirect to /home if not logged in
+    return NextResponse.redirect(new URL('/home', request.url)); 
   }
 
-  // Allow other requests to continue
   return NextResponse.next();
 }
