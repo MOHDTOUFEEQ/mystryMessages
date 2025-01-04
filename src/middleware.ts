de5bb54fdad576a997a8a7836cc9d40feff51c6d
GroupResearchProject <mohdtoufeeq1447@gmail.com>
2025-01-04 23:49:53 +0000

    fixing ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*', 
+    '/u/:username'],
 };
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('auth-token');
   const url = request.nextUrl;
-  if (!token && url.pathname.startsWith('/u')) {
-    return NextResponse.next();
+  if (url.pathname.startsWith('/u')) {
+    return NextResponse.next(); // Allow access
   }
   if (token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/verify') || url.pathname === '/')) {
     return NextResponse.redirect(new URL('/dashboard', request.url)); 
